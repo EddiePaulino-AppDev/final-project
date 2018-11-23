6ab0433670ced8893a6b2af43d44bb175a0e746f
epaulino <eddie.paulino5@gmail.com>
2018-11-23 20:30:10 +0000

    Fix migrations 2

--- db/migrate/20181108054636_add_title_to_sections2.rb
+++ db/migrate/20181108054636_add_title_to_sections2.rb
@@ -1,5 +1,5 @@
 class AddTitleToSections2 < ActiveRecord::Migration[5.1]
-  def change
-          add_column :sections, :title, :string
-  end
+  #def change
+   #       add_column :sections, :title, :string
+  #end
 end
