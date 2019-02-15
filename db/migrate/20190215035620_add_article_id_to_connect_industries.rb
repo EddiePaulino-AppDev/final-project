84da65aa2d28a86693b657dd66d918878d5d9fa3
epaulino <eddie.paulino5@gmail.com>
2019-02-15 13:49:45 +0000

    Fix migrations6

--- db/migrate/20190215035620_add_article_id_to_connect_industries.rb
+++ db/migrate/20190215035620_add_article_id_to_connect_industries.rb
@@ -1,4 +1,4 @@
-class AddArticleIdToConnectIndustry < ActiveRecord::Migration[5.1]
+class AddArticleIdToConnectIndustries < ActiveRecord::Migration[5.1]
   def change
     add_column :connect_industries, :article_id, :integer
   end
