2412f514967b386fcf54a89d2501fa02c3ccab1f
epaulino <eddie.paulino5@gmail.com>
2019-03-19 18:22:40 +0000

    3settings ckeditor 3-19

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
+//= link_tree ../javascripts/ckeditor
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link ckeditor/application.css
+//= link ckeditor/application.js
