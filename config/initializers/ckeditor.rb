e377df6f58b201c26c1fd7ab2d9b3e2932398886
epaulino <eddie.paulino5@gmail.com>
2019-03-19 23:19:47 +0000

    4settings ckeditor 3-19

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -49,10 +49,10 @@ Ckeditor.setup do |config|
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)
-   config.cdn_url = '//cdn.ckeditor.com/4.11.2/full/ckeditor.js'
+  #config.cdn_url = '//cdn.ckeditor.com/4.11.2/full/ckeditor.js'
 
   # JS config url
   # Used when CKEditor CDN enabled
   # By default: "ckeditor/config.js"
-   config.js_config_url = 'ckeditor/config.js'
+   config.js_config_url = 'ckeditor/ckeditor_config.js'
 end
