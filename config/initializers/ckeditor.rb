45acaf2197624aa5e57aa92e8ad3cc644c39233f
epaulino <eddie.paulino5@gmail.com>
2019-03-20 02:14:47 +0000

    try fixing ckeditor 5

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -49,10 +49,10 @@ Ckeditor.setup do |config|
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)
-  #config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
+  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
 
   # JS config url
   # Used when CKEditor CDN enabled
   # By default: "ckeditor/config.js"
-   #config.js_config_url = 'ckeditor/config.js'
+   config.js_config_url = 'ckeditor/config.js'
 end
