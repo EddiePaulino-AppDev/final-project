bd7177ffffae0c43b3917fe0cc41cfeb516ae8d0
epaulino <eddie.paulino5@gmail.com>
2019-03-26 04:07:37 +0000

    try ckeditor change to production.rb

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
-//= link_tree ../javascripts/ckeditor
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-//= link ckeditor/application.css
-//= link ckeditor/application.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( ckeditor/config.js)
-Rails.application.config.assets.precompile += %w( custom_styling.css )
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( ckeditor/*)
+#Rails.application.config.assets.precompile += %w( custom_styling.css )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
